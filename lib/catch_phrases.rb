259ada4e65c07be36328d1cb94f73b18a1b57d20
Serkan Sahin <ssahin55@outlook.com>
2020-08-18 19:13:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
 
-def toadstool
-  puts status
-end
+
+
+
+
+
+
+
+
+
+
+
+# def mario
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#   puts phrase
+# end
+
+# def toadstool
+#   puts status
+# end
